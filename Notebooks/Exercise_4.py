e29b589f6c3e4af52dcd5ab63a1ea78399019ca1
bwanya <Bwanyabrian@gmail.com>
2019-03-12 10:40:09 +0300

    Python_exercises.2

--- Notebooks/Exercise_4.py
+++ Notebooks/Exercise_4.py
@@ -1,13 +1,27 @@
 #!/home/user/miniconda3/bin/python
 
-## Create a while loop that starts with x = 0 and increments x until x is equal to 5. Each iteration should print to the console
+## Question_1 Creating a while loop that starts with x = 0 and increments x until x is equal to 5. Each iteration should print to the console
 
 x = 0
-while x < 5:
+while x <= 5:
     print(x+1)
     x = x+1
-    print("All systems go")
+    print("Ready for boarding")
     
     
-## Question 2:loop above skips printing x = 5 to the console but will print values of x from 6 to 10
+## Question_2:loop above skips printing x = 5 to the console but will print values of x from 6 to 10
 
+for x in range(10):
+    if x==4:
+        print("",x)
+        continue
+    print("Ready for boarding",x)
+    
+    
+## Question_3:Creating a for loop that prints values from 4 to 10 to the console
+
+for x in range(10):
+    if x<3:
+        print("",x)
+        continue
+    print("Ready for boarding",x)
---
+++ Notebooks/Exercise_5.py
@@ -0,0 +1,6 @@
+#!/home/user/miniconda3/bin/python
+
+## Calculating the % GC content in 'AAGGGCTTAGCTTAATTAAAGTGGCTGATTTGCGTTCAGTTGATGCAGAGTGGGGTTTTGCAGTCCTTA' using functions.
+
+
+
